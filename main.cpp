1cc1b8b41faaba11d0984edc0350b1eb6754db65
Nam Nguyễn <nguyenphinam2k2@gmail.com>
2023-02-19 00:28:49 +0700

    add bot

--- main.cpp
+++ main.cpp
@@ -3,19 +3,14 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 900;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 450;
+const int SCREEN_HEIGHT = 800;
 
-// //The window we'll be rendering to
-// SDL_Window* gWindow = NULL;
-// //The surface contained by the window
-// SDL_Surface* gScreenSurface = NULL;
-// //The image we will load and show on the screen
-
+const int FRAME_RATE = 60;
+const int FRAME_TIME = 1000 / FRAME_RATE;
 
 
 //Loads individual image as texture
@@ -32,20 +27,14 @@ SDL_Texture* gTexture = NULL;
 SDL_Texture* gBackground = NULL;
 
 
-
-SDL_Texture* loadTexture( std::string path )
-{
+SDL_Texture* loadTexture( std::string path ) {
 	//The final texture
 	SDL_Texture* newTexture = NULL;
-
 	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	if( loadedSurface == NULL )
-	{
 		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
-	else
-	{
+	else{
 		//Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
 		if( newTexture == NULL )
@@ -56,7 +45,6 @@ SDL_Texture* loadTexture( std::string path )
 		//Get rid of old loaded surface
 		SDL_FreeSurface( loadedSurface );
 	}
-
 	return newTexture;
 }
 
@@ -143,30 +131,107 @@ int main( int argc, char* args[] ) {
         bool game_running = true;
         SDL_Event e;
 
+		int player1Point = 0;
+		int player2Point = 0;
 
         SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
         SDL_RenderClear( gRenderer );
+        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0x00 );        
         
-        
-        SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+        SDL_Rect ballRect = {215, 390, 20, 20};
+        SDL_Rect player1Rect = {0, 780, 150, 20 };
+        SDL_Rect player2Rect = {250, 20, 150, 20};
+
         SDL_Rect backgroundRect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
         SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );        
+                
+        int acceX = 5;
+        int acceY = 5;
+		int touchNum = 0;
+
         while(game_running) {
+            Uint32 startTime = SDL_GetTicks();
+			const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+			int acceeX = 0;
             while( SDL_PollEvent( &e ) != 0 ){
                 if( e.type == SDL_QUIT ) {
                     game_running = false;
                 }
             }
-            // SDL_BlitSurface( gBackground, NULL, gScreenSurface, NULL );
-            // SDL_UpdateWindowSurface( gWindow );
+
+			if (state[SDL_SCANCODE_LEFT]){
+				player1Rect.x -= 15;
+			}
+			else if (state[SDL_SCANCODE_RIGHT]){
+				player1Rect.x += 15;
+			}
+
+			if (touchNum == 5){
+				if (acceX > 0){
+					acceX += 1;
+				}
+				else if ( acceX < 0)
+					acceX -= 1;
+
+
+				if (acceY > 0){
+					acceY += 1;
+				}
+				else if ( acceY < 0)
+					acceY -= 1;
+
+				touchNum = 0;
+			}
+            ballRect.x += acceX;
+            ballRect.y += acceY;
+			
+			if (SDL_HasIntersection(&ballRect, &player1Rect) || SDL_HasIntersection(&ballRect, &player2Rect))
+            {
+				acceY = acceY * -1;
+				acceX = acceX + acceeX;
+				touchNum += 1;
+			}    
+			else if (ballRect.x >= SCREEN_WIDTH || ballRect.x <= 0) {
+                acceX = acceX * -1;
+            	touchNum += 1;
+			}
             
-            fillRect.x = fillRect.x + 1;
-            
+			if (ballRect.y >= SCREEN_HEIGHT){
+                player2Point++;
+				acceeX = 5;
+				acceY = 5;
+				cout << "Play 1: " << player1Point << "- " << "Play 2: " << player2Point << endl;
+				ballRect = {215, 390, 20, 20};
+			}
+			if (ballRect.y <= 0 ){
+				player1Point++;
+				acceX = 5;
+				acceY = 5;
+				cout << "Play 1: " << player1Point << " - " << "Play 2: " << player2Point << endl;
+				ballRect = {215, 390, 20, 20};
+			}
+
+            if (ballRect.x != player2Rect.x){
+				if (ballRect.x - player2Rect.x > 18)
+					player2Rect.x += 18;
+				if (ballRect.x - player2Rect.x < 18)
+					player2Rect.x -= 18;
+            }    
+
             SDL_RenderCopy(gRenderer, gBackground, NULL, &backgroundRect);
-            SDL_RenderFillRect( gRenderer, &fillRect );
-            cout << fillRect.x << endl;
-            
+            SDL_RenderFillRect(gRenderer, &player1Rect );
+            SDL_RenderFillRect(gRenderer, &ballRect );
+            SDL_RenderFillRect(gRenderer, &player2Rect);
+
+			// cout << player1Point << " " << player1Point << endl;
             SDL_RenderPresent(gRenderer);
+
+            Uint32 frameTime = SDL_GetTicks() - startTime;
+            if (frameTime < FRAME_TIME)
+            {
+                SDL_Delay(FRAME_TIME - frameTime);
+            }
         }              
 	}
 	//Free resources and close SDL
--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 g++ -Isrc/Include -Lsrc/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2  -lSDL2_image
 main.exe
+
